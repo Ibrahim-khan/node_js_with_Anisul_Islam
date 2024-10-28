fcf275cdff4e3a67e25596c8b9c8b7a778809077
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-10-28 20:53:49 +0600

    node_js_with_Anisul_Islam

---
+++ module_basic/index.js
@@ -0,0 +1,5 @@
+const students = require('./student');
+
+console.log(students.getName());
+console.log(students.getAge());
+console.log(students.cgpa);
---
+++ module_basic/student.js
@@ -0,0 +1,16 @@
+const getName = () => {
+    return "Md Ibrahim Khan"
+};
+
+const getAge = () => {
+    return "25"
+}
+
+const cgpa = 3.5;
+
+
+module.exports = {
+    getName,
+    getAge,
+    cgpa
+}
