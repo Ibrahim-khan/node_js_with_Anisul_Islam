bb3238b7a006a4106352dda8b22ca20a49305644
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-11-04 15:44:47 +0600

    node_js_with_Anisul_Islam

--- http_routing/index.js
+++ http_routing/index.js
@@ -1,11 +1,34 @@
-const http = require('fs');
+const http = require('http');
+const fs = require('fs');
 const PORT = 3000;
 const hostName = '127.0.0.1';
 
-const server = http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
+
     if (req.url === "/") {
-        fs.readFile("index.html", (err, data) => {
+        fs.readFile("index", (err, data) => {
             res.writeHead(200, {"Content-Type":"text/html"});
+            res.write(data);
+            res.end();
+        });
+    }
+    else if (req.url === "/about") {
+        fs.readFile("about.html", (err, data) => {
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.write(data);
+            res.end();
+        });
+    }
+    else if (req.url === "/contact") {
+        fs.readFile("contact.html", (err, data) => {
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.write(data);
+            res.end();
+        });
+    }
+    else {
+        fs.readFile("error", (err, data) => {
+            res.writeHead(404, {"Content-Type":"text/html"});
             res.write(data);
             res.end();
         });
