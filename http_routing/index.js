2b1650265c50122f60edc3690906c9aa2a58187e
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-11-05 20:25:17 +0600

    node_js_with_Anisul_Islam

--- http_routing/index.js
+++ http_routing/index.js
@@ -1,40 +1,11 @@
 const http = require('http');
-const fs = require('fs');
 const PORT = 3000;
 const hostName = '127.0.0.1';
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req,res) => {
+    res.end("Welcome");
+})
 
-    if (req.url === "/") {
-        fs.readFile("index", (err, data) => {
-            res.writeHead(200, {"Content-Type":"text/html"});
-            res.write(data);
-            res.end();
-        });
-    }
-    else if (req.url === "/about") {
-        fs.readFile("about.html", (err, data) => {
-            res.writeHead(200, {"Content-Type":"text/html"});
-            res.write(data);
-            res.end();
-        });
-    }
-    else if (req.url === "/contact") {
-        fs.readFile("contact.html", (err, data) => {
-            res.writeHead(200, {"Content-Type":"text/html"});
-            res.write(data);
-            res.end();
-        });
-    }
-    else {
-        fs.readFile("error", (err, data) => {
-            res.writeHead(404, {"Content-Type":"text/html"});
-            res.write(data);
-            res.end();
-        });
-    }
-});
-
-server.listen(PORT, hostName, () => {
-    console.log(`Server is running at http://${hostName}:${PORT}`);
+server.listen(PORT, hostName, ()=> {
+    console.log(`server is runnging at http://${hostName}:${PORT}`);
 });
